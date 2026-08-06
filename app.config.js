1545149825f73c1234c269c0e48f2a114bcd5016
sachinelavarasan <sachinelavarasan321@gmail.com>
2026-08-06 15:58:44 +0530

    feat: update app and package versions to 2.0.6

--- app.config.js
+++ app.config.js
@@ -21,7 +21,7 @@ export default {
   "expo": {
     "name": isDev ? "Expensify Dev" : "Expensify",
     "slug": "expensify",
-    "version": "2.0.3",
+    "version": "2.0.6",
     "orientation": "portrait",
     "icon": "./assets/images/icon-themed.png",
     "scheme": isDev ? "expensify-dev" : "expensify",
